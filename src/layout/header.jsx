80e72d6373b5f35acfaa48a5bff323d66ccc8a4a
Krishna Kumar <krishnakumar882638@gmail.com>
2026-05-23 21:45:22 +0530

    all new changes and page updated

---
+++ src/layout/header.jsx
@@ -0,0 +1,11 @@
+import '../layout/headerStyle.scss';
+
+const Header = () => {
+    return(
+        <>
+                <h2 className='header'>header testing</h2>
+        </>
+    )
+}
+
+export default Header;
